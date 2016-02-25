95c0470caa84ffbdcf99d7228b4cb0cb529bae49
Derek King <king.dm49@gmail.com>
2016-02-26 00:48:46 +1100

    Initial commit

---
+++ main.py
@@ -0,0 +1,329 @@
+from glob import glob
+from os import chdir
+from tkinter import *
+from tkinter import ttk, filedialog, messagebox
+
+from mutagen.mp4 import MP4, MP4StreamInfoError
+
+from utils import get_external_tags
+
+__author__ = 'Derek'
+
+
+TAGS = [["Title:\t", "\xa9nam"], ["Album:\t", "\xa9alb"], ["Artist:\t", "\xa9ART"],
+        ["Track:\t", "trkn"], ["Year:\t", "\xa9day"], ["Genre:\t", "\xa9gen"]]
+OTHER_TAGS = {"aART": "Album artist:", "\xa9wrt": "Composer:", "\xa9cmt": "Comment:",
+              "desc": "Description:", "purd": "Purchase date:", "\xa9grp": "Grouping:",
+              "\xa9lyr": "Lyrics:", "purl": "Podcast URL:", "egid": "Podcast episode GUID:",
+              "catg": "Podcast category:", "keyw": "Podcast keywords:",
+              "\xa9too": "Encoded by:", "cprt": "Copyright:", "soal": "Album sort order:",
+              "soaa": "Album artist sort order:", "soar": "Artist sort order:",
+              "sonm": "Title sort order:", "soco": "Composer sort order:",
+              "sosn": "Show sort order:", "tvsh": "Show name:", "cpil": "Compilation:",
+              "pgap": "Gapless album:", "pcst": "Podcast:", "disk": "Disc number, total:",
+              "tmpo": "Tempo/BPM, 16 bit int:"}
+GENRES = open("genres.txt").read().splitlines()
+
+
+class CurrentInfo:
+    class CurrentEntry(ttk.Entry):
+        def __init__(self, *args, **kwargs):
+            ttk.Entry.__init__(self, *args, **kwargs)
+            self.state(["readonly"])
+
+    def __init__(self, parent, column):
+        self.var_list = [StringVar() if i is not 3 else [StringVar(), StringVar()] for i in range(len(TAGS))]
+
+        ttk.Label(parent, text="Current:").grid(column=column, row=0, sticky=W)
+
+        title = self.CurrentEntry(parent, textvariable=self.var_list[0])
+        title.grid(column=column, row=1, sticky=(W, E))
+
+        album = self.CurrentEntry(parent, textvariable=self.var_list[1])
+        album.grid(column=column, row=2, sticky=(W, E))
+
+        artist = self.CurrentEntry(parent, textvariable=self.var_list[2])
+        artist.grid(column=column, row=3, sticky=(W, E))
+
+        # Track
+        track_frame = ttk.Frame(parent, padding=(5, 0))
+        track_frame.grid(column=column, row=4, sticky=W)
+        track = self.CurrentEntry(track_frame, width=3, textvariable=self.var_list[3][0])
+        track.grid(column=0, row=0, sticky=(W, E))
+        # /
+        ttk.Label(track_frame, text="/").grid(column=1, row=0)
+        # Total Tracks
+        track_total = self.CurrentEntry(track_frame, width=3, textvariable=self.var_list[3][1])
+        track_total.grid(column=2, row=0, sticky=(W, E))
+
+        year = self.CurrentEntry(parent, width=5, textvariable=self.var_list[4])
+        year.grid(column=column, row=5, sticky=W)
+
+        genre = self.CurrentEntry(parent, textvariable=self.var_list[5])
+        genre.grid(column=column, row=6, sticky=(W, E))
+
+
+class NewEntry:
+    class NumEntry(ttk.Entry):
+        def __init__(self, length, min_val, max_val, *args, **kwargs):
+            ttk.Entry.__init__(self, *args, **kwargs)
+            self.length = length
+            self.min_val = min_val
+            self.max_val = max_val
+            self.configure(width=self.length + 1, validate="key",
+                           validatecommand=(self.register(self.on_validate), "%P"))
+
+        def on_validate(self, new_value):
+            if new_value.strip() == "":
+                return True
+            try:
+                value = int(new_value)
+                if value < self.min_val or value > self.max_val or len(str(value)) > self.length:
+                    raise ValueError
+            except ValueError:
+                self.bell()
+                return False
+
+            return True
+
+    def __init__(self, parent, column, select_var, name):
+        self.var_list = [StringVar() if i is not 3 else [StringVar(), StringVar()] for i in range(len(TAGS))]
+
+        ttk.Label(parent, text=name).grid(column=column, row=0, sticky=W)
+
+        self.title = ttk.Entry(parent, textvariable=self.var_list[0])
+        self.title.grid(column=column, row=1, sticky=(W, E))
+
+        album = ttk.Entry(parent, textvariable=self.var_list[1])
+        album.grid(column=column, row=2, sticky=(W, E))
+
+        artist = ttk.Entry(parent, textvariable=self.var_list[2])
+        artist.grid(column=column, row=3, sticky=(W, E))
+
+        #  Track
+        track_frame = ttk.Frame(parent, padding=(5, 0))
+        track_frame.grid(column=column, row=4, sticky=W)
+        track = self.NumEntry(2, 0, 99, track_frame, textvariable=self.var_list[3][0])
+        track.grid(column=0, row=0, sticky=(W, E))
+        # /
+        ttk.Label(track_frame, text="/").grid(column=1, row=0)
+        # Total Tracks
+        track_total = self.NumEntry(2, 0, 99, track_frame, textvariable=self.var_list[3][1])
+        track_total.grid(column=2, row=0, sticky=(W, E))
+
+        year = self.NumEntry(4, 0, 9999, parent, textvariable=self.var_list[4])
+        year.grid(column=column, row=5, sticky=W)
+
+        genre = ttk.Combobox(parent, textvariable=self.var_list[5], values=GENRES)
+        genre.grid(column=column, row=6, sticky=(W, E))
+
+        select = ttk.Radiobutton(parent, variable=select_var, value=name)
+        select.grid(column=column, row=7)
+
+
+class MainMenu(Menu):
+    def __init__(self, close):
+        super(MainMenu, self).__init__()
+
+        self.option_add("*tearOff", FALSE)
+
+        file_menu = Menu(self)
+        file_menu.add_command(label="Exit", command=close)
+
+        help_menu = Menu(self)
+        help_menu.add_command(label="About",
+                              command=lambda: messagebox.showinfo("About", "This is an Mp4 tagger"))
+
+        self.add_cascade(menu=file_menu, label="File")
+        self.add_cascade(menu=help_menu, label="Help")
+
+
+class Gui(Tk):
+    def __init__(self):
+        # TODO: add url entry
+        # TODO: add progress bar and threads
+        super(Gui, self).__init__()
+        self.title("M4a Tagger")
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+        main_menu = MainMenu(self.quit)
+        self.config(menu=main_menu)
+
+        self.songs = []
+        self.total_songs = 0
+        self.current_song = MP4()
+        self.extra_tags = dict()
+
+        mainframe = ttk.Frame(self, padding=(3, 3, 0, 0))
+        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        mainframe.bind("<1>", lambda event: mainframe.focus_set())
+
+        # Row Labels
+        for i, item in enumerate(TAGS):
+            ttk.Label(mainframe, text=item[0]).grid(column=0, row=i + 1, sticky=E)
+
+        self.current_info = CurrentInfo(mainframe, 1)
+        mainframe.columnconfigure(1, weight=1, uniform="a")
+
+        # Extra tags
+        self.extra_tags_var = StringVar()
+        ttk.Label(mainframe, text="The following tags will be removed:") \
+            .grid(column=1, row=7, sticky=W)
+        self.extra_tags_frame = ttk.Frame(mainframe, padding=(5, 0), borderwidth=2, relief="sunken")
+        self.extra_tags_frame.grid(column=1, columnspan=2, row=8, sticky=(W, E), padx=5)
+        ttk.Label(self.extra_tags_frame, textvariable=self.extra_tags_var, font="TkFixedFont") \
+            .grid(column=0, row=0, sticky=(W, E))
+
+        self.entry_select = StringVar(value="Dbpedia:")
+
+        self.dbpedia_entry = NewEntry(mainframe, 2, self.entry_select, "Dbpedia:")
+        self.dbpedia_entry.title.focus()
+        mainframe.columnconfigure(2, weight=1, uniform="a")
+
+        self.wiki_entry = NewEntry(mainframe, 3, self.entry_select, "Wikipedia:")
+        mainframe.columnconfigure(3, weight=1, uniform="a")
+
+        self.discogs_entry = NewEntry(mainframe, 4, self.entry_select, "Discogs:")
+        mainframe.columnconfigure(4, weight=1, uniform="a")
+
+        # Buttons
+        button_frame = ttk.Frame(mainframe, padding=(5, 10, 0, 0))
+        button_frame.grid(column=2, columnspan=3, row=100, sticky=E)
+        #
+        self.progress = StringVar(value="Song 0 of 0")
+        ttk.Label(button_frame, textvariable=self.progress).grid(column=0, row=0)
+        #
+        self.save_button = ttk.Button(button_frame, text="Save", command=self.save)
+        self.save_button.grid(column=1, row=0)
+        self.save_button.state(["disabled"])
+        #
+        self.next_song_button = ttk.Button(button_frame, text="Next Song", command=self.next_song)
+        self.next_song_button.grid(column=2, row=0)
+        self.next_song_button.state(["disabled"])
+        #
+        self.browse_button = ttk.Button(button_frame, text="Open Folder", command=self.browse)
+        self.browse_button.grid(column=3, row=0)
+
+        for child in mainframe.winfo_children():
+            if type(child).__name__ != "Frame":
+                child.grid_configure(padx=5, pady=5)
+
+        ttk.Sizegrip(self).grid(column=999, row=999, sticky=(S, E))
+
+    def set_vars(self):
+        def get_extra_tags():
+            if "covr" in self.extra_tags:
+                del self.extra_tags["covr"]
+            if self.extra_tags:
+                # TODO: handle bools instead of just self.extra_tags[tag][0]
+                return "\n".join([OTHER_TAGS.get(tag, tag).ljust(24) + str(self.extra_tags[tag][0])
+                                  for tag in self.extra_tags])
+            else:
+                return "None"
+
+        def set_var_list(var_list, tag_list):
+            for i, var in enumerate(var_list):
+                if i is 3:
+                    var[0].set(tag_list[i][0])
+                    if tag_list[i][1]:
+                        var[1].set(tag_list[i][1])
+                    else:
+                        var[1].set("")
+                else:
+                    var.set(tag_list[i])
+
+        self.extra_tags = dict(self.current_song.tags)
+        song_tags = [self.extra_tags.pop(tag[1], "None")[0] for tag in TAGS]
+
+        set_var_list(self.current_info.var_list, song_tags)
+
+        self.extra_tags_var.set(get_extra_tags())
+
+        external_tags = get_external_tags(song_tags[0])
+        set_var_list(self.dbpedia_entry.var_list, external_tags[0])
+        set_var_list(self.wiki_entry.var_list, external_tags[1])
+        set_var_list(self.discogs_entry.var_list, external_tags[2])
+
+        self.progress.set(value="Song "+str(self.total_songs-len(self.songs))+" of "+str(self.total_songs))
+
+    def open_next_song(self):
+        while self.songs:
+            try:
+                self.current_song = MP4(self.songs.pop())
+            except MP4StreamInfoError as e:
+                print(repr(e))
+            else:
+                return True
+        return False
+
+    def browse(self):
+
+        directory = filedialog.askdirectory(parent=self, mustexist=True)
+        if not directory:
+            return
+        try:
+            chdir(directory)
+        except OSError as e:
+            messagebox.showerror(title="Error", message=repr(e))
+            return
+
+        self.songs = glob("*.m4a")
+        if not self.songs:
+            messagebox.showerror(title="Error", message="No .m4a files in the given directory")
+            return
+        self.songs.sort()
+        self.songs.reverse()
+        self.total_songs = len(self.songs)
+
+        if not self.open_next_song():
+            messagebox.showerror(title="Error", message="No valid .m4a files in the given directory")
+            return
+
+        self.set_vars()
+
+        if self.songs:
+            self.next_song_button.state(["!disabled"])
+            self.save_button.state(["!disabled"])
+
+    def save(self):
+
+        if self.extra_tags:
+            for tag in self.extra_tags:
+                del self.current_song[tag]
+
+        if self.entry_select == "Dbpedia:":
+            var_save = self.dbpedia_entry.var_list
+        elif self.entry_select == "Wikipedia:":
+            var_save = self.wiki_entry.var_list
+        else:
+            var_save = self.discogs_entry.var_list
+
+        for i, var in enumerate(var_save):
+            if i is 3:
+                self.current_song[TAGS[i][1]] = [(int("0" + var[0].get()), int("0" + var[1].get()))]
+            else:
+                self.current_song[TAGS[i][1]] = [var.get()]
+
+        try:
+            self.current_song.save()
+        except Exception as e:
+            messagebox.showerror(title="Error", message="Couldn't save tags to file",
+                                 detail=repr(e))
+        else:
+            self.save_button.state(["disabled"])
+
+    def next_song(self):
+        if not self.open_next_song():
+            messagebox.showerror(title="Error", message="No more valid .m4a files in directory")
+            self.next_song_button.state(["disabled"])
+            return
+
+        if not self.songs:
+            self.next_song_button.state(["disabled"])
+        self.save_button.state(["!disabled"])
+
+        self.set_vars()
+
+
+if __name__ == '__main__':
+    Gui().mainloop()
---
+++ utils.py
@@ -0,0 +1,176 @@
+import re
+from json import loads
+from webbrowser import open_new_tab
+
+import discogs_client  # https://github.com/discogs/discogs_client
+from lxml import html
+from requests import get
+
+import wptools  # https://github.com/siznax/wptools
+
+# get info from wiki infobox (also open wiki page in default browser)
+# use songname and artist to get song info from discogs
+# use album from wiki to get track info from discogs
+# allow choice between both
+
+discogs = discogs_client.Client('M4a_tagger/0.9', user_token="kbbWNbNREvGSEWWaPJRPQSmeLBuHQxvzGMuLJsQr")
+
+
+def dbpedia(input_title, url=""):
+    def query(q, site="http://dbpedia.org/sparql", parameters=None):
+        if not parameters:
+            parameters = {"query": q}
+        try:
+            resp = get(site, params=parameters, headers={"Accept": "application/json"})
+        except Exception as e:
+            print("0", repr(e))
+            raise
+        return resp.text
+
+    def get_property(data):
+        pattern = """
+                select ?{property} where {{
+                <{uri}> {a}:{property} ?{property} }}"""
+        json = loads(query(pattern.format(**data)))
+        prop = json["results"]["bindings"][0][data["property"]]["value"]
+        return prop
+
+    tags = [""] * 5
+
+    if url:
+        dbpedia_uri = "http://dbpedia.org/resource/" + url.split("/")[-1]
+    else:
+        try:
+            dbpedia_uri = loads(query("",
+                                      site="http://lookup.dbpedia.org/api/search/KeywordSearch",
+                                      parameters={"QueryClass": "MusicalWork",
+                                                  "QueryString": input_title}))["results"][0]["uri"]
+            url = get_property({"a": "foaf", "property": "isPrimaryTopicOf", "uri": dbpedia_uri})
+        except Exception as e:
+            print("1", repr(e))
+            return "", tags
+
+    vars = ["name", "album", "artist", "year", "genre"]
+    pattern = """
+              select * where {{
+               <{0}> foaf:name ?name .
+               <{0}> dbo:album ?album .
+               <{0}> dbo:musicalArtist ?artist .
+               <{0}> dbo:releaseDate ?year .
+               <{0}> dbo:genre ?genre .
+              }} limit 1
+              """
+
+    try:
+        json = loads(query(pattern.format(dbpedia_uri)))
+        properties = json["results"]["bindings"][0]
+
+        for i, var in enumerate(vars):
+            value = properties[var]["value"]
+            if properties[var]["type"] == "uri":
+                try:
+                    tags[i] = get_property({"a": "foaf", "property": "name", "uri": value})
+                except IndexError:
+                    tags[i] = get_property({"a": "rdfs", "property": "label", "uri": value})
+            else:
+                tags[i] = value
+    except Exception as e:
+        print("2", repr(e))
+
+    tags[3] = tags[3].split("-")[0]
+
+    return url, tags
+
+
+def parse_wiki(title):
+    if not title:
+        return [""] * 5
+
+    labels = ["Name",
+              "from Album",
+              "Artist",
+              "Released",
+              "Genre"]
+    alt_labels = ["Name",
+                  "Album",
+                  "Artist",
+                  "Released",
+                  "Genre"]
+
+    data = wptools.get_parsetree(title, lead=False, test=False, wiki="http://en.wikipedia.org")
+    info_box = wptools.infobox(data)
+
+    tags = [info_box.get(label, "") for label in labels]
+    for i in range(len(tags)):
+        if not tags[i]:
+            tags[i] = info_box.get(alt_labels[i], "")
+        if tags[i].startswith("<template"):
+            text_list = html.fromstring(tags[i]).xpath("//text()")
+            for text in text_list:
+                if text.getparent().tag == "value":
+                    tags[i] = text
+                    break
+        tags[i] = tags[i].replace("[", "").replace("]", "")
+
+    year = re.search("(\d{4})", tags[3])
+    if year:
+        tags[3] = year.group(1)
+    return tags
+
+
+def parse_discogs(title, album):
+    if not (title and album):
+        return (0, 0), ["", "", "", (0, 0), "", ""]
+
+    discogs_song = discogs.search(title, type="release")[0]
+    discogs_album = discogs.search(album, type="release")[0]
+
+    # Added to force loading of titles TODO: possible bug, possibly fixed?
+    # _ = discogs_song.artists[0].name
+    # _ = discogs_album.artists[0].name
+    # ###
+
+    try:
+        track_num = [track.title for track in discogs_album.tracklist].index(discogs_song.master.title) + 1
+    except ValueError:
+        track_num = 0
+    track_total = len(discogs_album.tracklist)
+    tracks = (track_num, track_total)
+    tags = [discogs_song.title,
+            discogs_album.title,
+            discogs_song.artists[0].name,
+            tracks,
+            str(discogs_song.year),
+            discogs_song.genres[0]]
+
+    return tracks, tags
+
+
+def get_external_tags(title, browser=True, wiki_page="", time=False):
+    from functions import Timer
+
+    with Timer(time):
+        wiki_page, tags_dbpedia = dbpedia(title, wiki_page)  # 6 secs, 3 s
+
+    if wiki_page and browser:
+        open_new_tab(wiki_page)
+
+    with Timer(time):
+        tags_wiki = parse_wiki(wiki_page.split("/")[-1])  # 3 secs
+
+    with Timer(time):
+        track_nums, tags_discogs = parse_discogs(tags_wiki[0], tags_wiki[1])  # 7 secs, 5 s
+
+    tags_wiki.insert(3, track_nums)
+    tags_dbpedia.insert(3, track_nums)
+
+    return tags_dbpedia, tags_wiki, tags_discogs
+
+
+if __name__ == '__main__':
+    external_tags = get_external_tags("Crush", wiki_page="https://en.wikipedia.org/wiki/Only_You_(Yazoo_song)",
+                                      time=True)
+    if external_tags:
+        print(external_tags[0])
+        print(external_tags[1])
+        print(external_tags[2])
